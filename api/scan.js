208925a14866299800d8de6b7bd8ac8b64ac2685
PK131101 <98271315+PK131101@users.noreply.github.com>
2026-03-04 09:03:37 +0530

    Update scan.js

--- api/scan.js
+++ api/scan.js
@@ -66,8 +66,8 @@ Rules:
       }
     });
 
-      const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${apiKey}`;
-
+     const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${apiKey}`;
+    
     const response = await fetch(url, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
